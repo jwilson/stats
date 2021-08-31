0346f8de0e5806eb4aa707f2e18bf615d96b3ecf
neilson <jwilson@users.noreply.github.com>
2021-08-31 11:00:49 -0400

    trying to get db going

--- stats/settings/development.py
+++ stats/settings/development.py
@@ -7,16 +7,20 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'postgres3',
-    #     'HOST': 'localhost',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': BASE_DIR + '/db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432,
+
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'NAME': BASE_DIR / 'db.sqlite3',
+    #     'NAME': BASE_DIR + '/db.sqlite3',
+    # }
 }
 
 try:
--- stats/settings/production.py
+++ stats/settings/production.py
@@ -8,10 +8,18 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': BASE_DIR + '/db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432,
+
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'NAME': BASE_DIR / 'db.sqlite3',
+    #     'NAME': BASE_DIR + '/db.sqlite3',
+    # }
 }
 
 try:
